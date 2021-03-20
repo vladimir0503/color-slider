8058d4c0be4929e8580be89e524a988a6ff46488
Vladimir <vovan0503@mail.ru>
2021-03-20 22:03:15 +0300

    Commit

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ $(() => {
         ];
         $.each(hex, function (nr, val) {
             if (val.length === 1) {
-                hex[nr] = "0" + val;
-            }
+                hex[nr] = `0${val}`;
+            };
         });
         return hex.join("").toUpperCase();
     };
