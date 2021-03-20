475c4455754592632dd15509e526417409ca5775
Vladimir <vovan0503@mail.ru>
2021-03-20 21:24:42 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+$(() => {
+    let currentPropElem = "background-color";
+
+    hexFromRGB = (r, g, b) => {
+        const hex = [
+            r.toString(16),
+            g.toString(16),
+            b.toString(16)
+        ];
+        $.each(hex, function (nr, val) {
+            if (val.length === 1) {
+                hex[nr] = "0" + val;
+            }
+        });
+        return hex.join("").toUpperCase();
+    };
+
+    function refreshSwatch() {
+        const red = $("#red").slider("value"),
+            green = $("#green").slider("value"),
+            blue = $("#blue").slider("value"),
+            hex = hexFromRGB(red, green, blue);
+        $("#swatch").css(currentPropElem, "#" + hex);
+        activeBtn();
+    };
+
+    function activeBtn() {
+        const [...btns] = $("button");
+        btns.forEach(btn => {
+            if (btn.id === currentPropElem) {
+                $(`#${btn.id}`).addClass("btnActive");
+            } else {
+                $(`#${btn.id}`).removeClass("btnActive");
+            };
+        });
+    };
+
+    $('#color').on('click', () => {
+        currentPropElem = 'color';
+        refreshSwatch();
+    });
+
+    $('#background-color').on('click', () => {
+        currentPropElem = 'background-color';
+        refreshSwatch();
+    });
+
+    $("#red, #green, #blue").slider({
+        orientation: "horizontal",
+        range: "min",
+        max: 255,
+        value: 127,
+        slide: refreshSwatch,
+        change: refreshSwatch
+    });
+    $("#red").slider("value", 255);
+    $("#green").slider("value", 140);
+    $("#blue").slider("value", 60);
+});
